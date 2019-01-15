561883cf4a50dd4388daf5793653e89d0fb74e6c
= <=>
2019-01-15 08:22:38 -0600

    add gtk

--- scripts/theme.sh
+++ scripts/theme.sh
@@ -9,7 +9,7 @@ sudo pacman -S --noconfirm arc-gtk-theme arc-icon-theme lxappearance
 mkdir -p /home/dev/.config/gtk-3.0 && cp /tmp/content/home/.config/gtk-3.0/settings.ini "$_"
 
 # GTK 2 settings
-cp /tmp/content/.gtkrc-2.0 /home/dev/ 
+cp /tmp/content/home/.gtkrc-2.0 /home/dev/ 
 
 # Icon settings
 mkdir -p /home/dev/.icons/default && cp /tmp/content/home/.icons/default/index.theme "$_"
