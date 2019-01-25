c3f8f3671d8ea2b7e1b249f46fc11d20d6cbb042
= <=>
2019-01-25 10:15:46 -0600

    add dotnet

--- scripts/dotnet.sh
+++ scripts/dotnet.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 # dotnet sdk and visual studio code
-sudo pacman -S --noconfirm dotnet-sdk code httpie
+sudo pacman -S --noconfirm dotnet-sdk code
 
 # opt out of telemetry
 echo "export DOTNET_CLI_TELEMETRY_OPTOUT=1" >> ~/.bashrc
