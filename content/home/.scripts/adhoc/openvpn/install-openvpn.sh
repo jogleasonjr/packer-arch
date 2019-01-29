577c45bc85aa000b553739e3799e37c099ee2e7f
= <=>
2019-01-29 14:31:30 -0600

    openvpn scripts with the DNS hack

---
+++ content/home/.scripts/adhoc/openvpn/install-openvpn.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# location of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# install openvpn
+sudo pacman -S --noconfirm openvpn
+
+# DNS is goofy with openvpn as noted here: https://wiki.archlinux.org/index.php/OpenVPN#Update_resolv-conf_script
+# this script places the above executable in the correct location
+chmod +x $DIR/update-resolv-conf.sh
+yes | cp -rf $DIR/update-resolv-conf.sh /etc/openvpn/
+
+printf "** IMPORTANT **
+append the following three lines to your openvpn config file for DNS to refresh when the vpn tunnel goes up and down:
+   script-security 2
+   up /etc/openvpn/update-resolv-conf.sh
+   down /etc/openvpn/update-resolv-conf.sh
+"
